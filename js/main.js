0a8cc23a7f1be4a5213e780961e3292ce525f86e
Denis Morev <morev.denis@gmail.com>
2022-06-20 22:44:47 +0300

    выполнить рефакторинг по замечаниям ревью

--- js/main.js
+++ js/main.js
@@ -1,12 +1,17 @@
-const OFFERS_NUMBER = 10;
-const MIN_LAT = 35.65;
-const MAX_LAT = 35.7;
-const MIN_LNG = 139.7;
-const MAX_LNG = 139.8;
-
+const OFFERS_COUNT = 10;
+const LATITUDE = {
+  MIN: 35.65,
+  MAX: 35.7,
+};
+const LONGITUDE = {
+  MIN: 139.7,
+  MAX: 139.8,
+};
 const OFFER_TYPES = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
-const CHECKIN_TIMES = ['12:00', '13:00', '14:00'];
-const CHECKOUT_TIMES = ['12:00', '13:00', '14:00'];
+const TIME = {
+  CHECKIN: ['12:00', '13:00', '14:00'],
+  CHECKOUT: ['12:00', '13:00', '14:00'],
+};
 const FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 const PHOTOS = [
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
@@ -29,36 +34,34 @@ function getRandomPositiveFloat(a, b, digits = 1) {
 
   const result = Math.random() * (upper - lower) + lower;
 
-  return +result.toFixed(digits);
+  return Number(result.toFixed(digits));
 }
 
 function getAvatarData(length) {
   const avatarUrlData = [];
 
-  for (let i = length; i >= 1; i--) {
-    avatarUrlData.push(`img/avatars/user${i < 10 ? '0' : ''}${i}.png`);
-    // if (i < 10) {
-    //   avatarUrlData.push(`img/avatars/user0${i}.png`);
-    // } else {
-    //   avatarUrlData.push(`img/avatars/user${i}.png`);
-    // }
+  for (let i = 1; i <= length; i++) {
+    avatarUrlData.push(`img/avatars/user${String(i).padStart(2, '0')}.png`);
   }
 
   return avatarUrlData;
 }
 
-const avatarData = getAvatarData(OFFERS_NUMBER);
+const avatarData = getAvatarData(OFFERS_COUNT);
+
+function getAvatar(index, avatars) {
+  return avatars[index];
+}
 
 function getRandomArrayElement(elements) {
   return elements[getRandomPositiveInteger(0, elements.length - 1)];
 }
 
 function getRandomLocation() {
-  const randomLocation = {
-    lat: getRandomPositiveFloat(MIN_LAT, MAX_LAT, 5),
-    lng: getRandomPositiveFloat(MIN_LNG, MAX_LNG, 5),
+  return {
+    lat: getRandomPositiveFloat(LATITUDE.MIN, LATITUDE.MAX, 5),
+    lng: getRandomPositiveFloat(LONGITUDE.MIN, LONGITUDE.MAX, 5),
   };
-  return randomLocation;
 }
 
 function getRandomFeatures(featuresData) {
@@ -75,18 +78,17 @@ function getRandomFeatures(featuresData) {
 
 function getRandomPhotosList(photosList) {
   const randomPhotosListLength = getRandomPositiveInteger(1, 5);
-  const randomPhotosList = Array.from(
+
+  return Array.from(
     { length: randomPhotosListLength },
     () => photosList[getRandomPositiveInteger(0, photosList.length - 1)],
   );
-
-  return randomPhotosList;
 }
 
-function createOffer(location) {
+function createOffer(id, location) {
   return {
     author: {
-      avatar: avatarData.pop(),
+      avatar: getAvatar(id, avatarData),
     },
     offer: {
       title: 'Апартаменты c видом на море',
@@ -95,8 +97,8 @@ function createOffer(location) {
       type: getRandomArrayElement(OFFER_TYPES),
       rooms: getRandomPositiveInteger(1, 10),
       guests: getRandomPositiveInteger(1, 5),
-      checkin: getRandomArrayElement(CHECKIN_TIMES),
-      checkout: getRandomArrayElement(CHECKOUT_TIMES),
+      checkin: getRandomArrayElement(TIME.CHECKIN),
+      checkout: getRandomArrayElement(TIME.CHECKOUT),
       features: getRandomFeatures(FEATURES),
       description: 'Апартаменты расположены в 30 минутах ходьбы от центра города',
       photos: getRandomPhotosList(PHOTOS),
@@ -108,6 +110,8 @@ function createOffer(location) {
   };
 }
 
-const offers = Array.from({ length: OFFERS_NUMBER }, () => createOffer(getRandomLocation()));
+const offers = Array.from({ length: OFFERS_COUNT }, (current, index) =>
+  createOffer(index, getRandomLocation()),
+);
 
 export { offers };
